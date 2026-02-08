33f85a8469f12a7b7a2f15413ece65233480ee38
Tanish Tyagi <Tanish.24BCS10263@sst.scaler.com>
2026-02-08 16:04:58 +0530

    Prepared for Vercel Deployment

--- HeartHealth/settings.py
+++ HeartHealth/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6*#plk7*tnfkfpp@ab34o4=tk5mww(tz@6=)p1d_6_w@%7ervu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
